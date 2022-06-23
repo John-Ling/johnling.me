6be42375484c3d0be8e3317468fcdc26a1eee77a
John Ling <johnlingbusiness@gmail.com>
2022-06-23 17:47:20 +0800

    Changed small details such as navbar and images

--- home.js
+++ home.js
@@ -1,4 +1,13 @@
-function test() 
+function get_age()
 {
-    alert(test);
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const day = today.getDate();
+    let age = year - 2005;
+    if (month < 7 || month == 8 && day < 7)
+    {
+    	age--;
+    }
+    return age;
 }
