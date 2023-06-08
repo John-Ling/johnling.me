5fb051c8558c2ce72ee8c2c6bd81b235038b1151
John Ling <johnlingbusiness@gmail.com>
2023-06-08 14:25:38 +0800

    Completed website overhaul

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,26 @@
 const get_age = () => {
-    const today = new Date()
-    const year = today.getFullYear()
-    const month = today.getMonth()
-    const day = today.getDate()
-    let age = year - 2005
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const day = today.getDate();
+    let age = year - 2005;
     if (month < 7 || month == 8 && day < 7) {
-        age--
+        age--;
     }
-    return age
+    return age;
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("age-text").innerHTML = get_age();
-    const languageIcons = document.querySelectorAll(".language-icon")
-    const languageIconsFadeOrder = [0, 1, 4, 2, 5, 3, 6, 7] // indexes of language icons
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.style.animation = "fade 2s ease"
-                observer.unobserve(entry.target)
-            }
-        })
-    })
-    languageIconsFadeOrder.forEach(index => {
-        // add later
-    })
+
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach(element => observer.observe(element));
 })
