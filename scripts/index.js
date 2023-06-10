fcb4585af8e7d3a7d8cb1f62c91f58f542984eb7
John Ling <johnlingbusiness@gmail.com>
2023-06-10 22:09:48 +0800

    Completed most of new look

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-function get_age() {
+const get_age = () => {
     const today = new Date()
     const year = today.getFullYear()
     const month = today.getMonth()
@@ -10,19 +10,16 @@ function get_age() {
     return age
 }
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("age-text").innerHTML = get_age()
-    const languageIcons = document.querySelectorAll(".language-icon")
-    const languageIconsFadeOrder = [0, 1, 4, 2, 5, 3, 6, 7] // indexes of language icons
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.style.animation = "fade 2s ease"
-                observer.unobserve(entry.target)
-            }
-        })
-    })
-    languageIconsFadeOrder.forEach(index => {
-        // add later
-    })
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach(element => observer.observe(element)); 
 })
--- scss/compile_sass.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sass --watch \
-index.scss:index.css \
-projects.scss:projects.css \
-charity.scss:charity.css \
-articles.scss:articles.css
