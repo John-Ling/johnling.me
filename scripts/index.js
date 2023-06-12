3103ceb3d08887bc39d9f90c15a1ca582692f1f9
John Ling <johnlingbusiness@gmail.com>
2023-06-12 11:20:13 +0800

    Corrected errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,25 @@
 const get_age = () => {
-    const today = new Date()
-    const year = today.getFullYear()
-    const month = today.getMonth()
-    const day = today.getDate()
-    let age = year - 2005
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const day = today.getDate();
+    let age = year - 2005;
     if (month < 7 || month == 8 && day < 7) {
-        age--
+        age--;
     }
-    return age
+    return age;
+}
+
+const get_years_programming = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    let years = year - 2019;
+    if (month < 9) {
+        years--;
+    }
+
+    return years;
 }
 
 const observer = new IntersectionObserver((entries) => {
@@ -19,7 +31,10 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("age-text").innerHTML = get_age()
+    const ageTexts = document.querySelectorAll(".age-text");
+    ageTexts.forEach(element => element.innerHTML = get_age());
+    const yearsTexts = document.querySelectorAll(".years-text");
+    yearsTexts.forEach(element => element.innerHTML = get_years_programming());
     const hiddenElements = document.querySelectorAll(".hidden");
     hiddenElements.forEach(element => observer.observe(element)); 
 })
