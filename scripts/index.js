9342ee4401ff8813178853353c8a101eb44fef22
John Ling <johnlingbusiness@gmail.com>
2023-06-15 16:20:09 +0800

    Further optimisation + SCSS refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -22,32 +22,10 @@ const get_years_programming = () => {
     return years;
 }
 
-const slideObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show-slide");
-        }
-    });
-});
-
-const fadeObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show-fade");
-        }
-    });
-})
-
 document.addEventListener("DOMContentLoaded", () => {
     const ageTexts = document.querySelectorAll(".age-text");
     ageTexts.forEach((element) => element.innerHTML = get_age());
 
     const yearsTexts = document.querySelectorAll(".years-text");
     yearsTexts.forEach((element) => element.innerHTML = get_years_programming());
-
-    const hiddenSlideElements = document.querySelectorAll(".hidden-slide");
-    hiddenSlideElements.forEach((element) => slideObserver.observe(element)); 
-
-    const hiddenFadeElements = document.querySelectorAll(".hidden-fade");
-    hiddenFadeElements.forEach((element) => slideObserver.observe(element));
-})
+});
