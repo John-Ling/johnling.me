2370160a28777a0d570814a3045b221fe4511b58
John <johnlingbusiness@gmail.com>
2024-11-25 14:47:11 +1100

    Added dropdown menu for navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -17,38 +17,47 @@ const Navbar = () => {
     { name: "Contact", target: "/contact"},
     { name: "Resume", target: "/resume"},
   ];
-
+ 
   // status of mobile menu
-  const [open, setOpen] = useState<boolean>(true);
+  const [open, setOpen] = useState<boolean>(false);
   
   const path: string = usePathname();
   return (
     <nav className="w-full bg-grey-light gap-x-4 gap-y-2 flex flex-col md:flex-row">
-      <a className="text-xl no-underline md:self-center" href="/">John Ling</a>
-      <div id="navbar"> 
-        <div className="bg-red">
-          <button className="md:hidden bg-orange"></button>
-        </div>
-        {open && 
-          <ul className="flex flex-col md:flex-row gap-x-2 gap-y-2 md:gap-x-2">
-          {links.map((link: NavLink) => {
-            let className: string = "w-max no-underline p-2 inline-block w-screen md:w-auto";
-            let ariaCurrent: any = undefined;
-
-            // apply special styles for active page
-            if (link.target === path) {
-              className += " border-orange md:border-b-2 border-l-2 md:border-l-0 bg-grey-dark md:bg-grey-light";
-              ariaCurrent = "page";
-            }
-
-            return <li className="active:bg-grey-dark">
-              <Link className={className} aria-current={ariaCurrent} href={link.target}>{link.name}</Link>
-            </li>
-          })}
-          </ul>
-        }
+      <span className="p-2 flex justify-between">
+        <a className="text-xl no-underline md:self-center" href="/">John Ling</a>
+        <button className="md:hidden bg-orange hover:bg-blue active:bg-red" onClick={() => setOpen(!open)}>Dropdown</button>
+      </span>
+      {/* desktop menu */}
+      <div className="hidden md:flex">
+        <NavbarMenu links={links} activeLink={path}/>
       </div>
+      
+
+      {/* mobile menu */}
+      {open && <NavbarMenu links={links} activeLink={path}/>}
     </nav>
+  )
+}
+
+const NavbarMenu: React.FC<{links: NavLink[], activeLink: string}> = ({links, activeLink}) => {
+  return (
+    <ul className="flex flex-col md:flex-row gap-x-2 gap-y-2 md:gap-x-2 absolute bg-grey-normal top-10 shadow-md">
+    {links.map((link: NavLink) => {
+      let className: string = "w-max no-underline p-2 inline-block w-screen md:w-auto";
+      let ariaCurrent: any = undefined;
+
+      // apply special styles for active page
+      if (link.target === activeLink) {
+        className += " border-orange md:border-b-2 border-l-2 md:border-l-0";
+        ariaCurrent = "page";
+      }
+
+      return <li key={link.name}>
+        <Link className={className} aria-current={ariaCurrent} href={link.target}>{link.name}</Link>
+      </li>
+    })}
+    </ul>
   )
 }
 
