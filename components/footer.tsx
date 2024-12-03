4baf62051dba49836c85b6fc4774853ff3c9a3b3
John <johnlingbusiness@gmail.com>
2024-12-04 09:10:11 +1100

    Removed svgs

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="flex flex-col p-5 items-center border-t-2 border-grey-light bg-grey-dark mt-auto">
       <h3>Made By <span className="text-orange">John Ling</span></h3>
-      <h4>Source Code at <Link href="https://github.com/John-Ling/johnling.me">Github</Link></h4>
+      <h4>Source Code at <a href="https://github.com/John-Ling/johnling.me">Github</a></h4>
       <Link href="/">Return Home</Link>
     </footer>
   )
