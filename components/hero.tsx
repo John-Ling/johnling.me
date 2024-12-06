f36d8b5ea1b56b4e3b8af647dcef1a3873a92e3a
John <johnlingbusiness@gmail.com>
2024-12-06 11:04:34 +1100

    Tried fixing flicker issue for slower connections

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,17 +1,27 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useLayoutEffect } from "react";
 import { evolve, conway_populate } from "./ascii-display/animations";
 import AsciiDisplay from "./ascii-display/ascii_display";
-
 import "/styles/globals.css";
 
-
-
 const Hero: React.FC<{asciiWidth: number, asciiHeight: number}> = ({asciiWidth, asciiHeight}) => {
-  const [grid, setGrid] = useState<string[][]>([[]]);
+
+  const create = () => {
+    const grid: string[][] = [];
+    for (let i = 0; i < asciiHeight; i++) {
+      const buffer: string[] = [];
+      for (let j = 0; j < asciiWidth; j++) {
+        buffer.push(' ');
+      }
+      grid.push(buffer);
+    }
+    return grid;
+  }
+
+  const [grid, setGrid] = useState<string[][]>(create);
   const [loaded, setLoaded] = useState<boolean>(false);
   const animationRequestID = useRef<number>(0);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setLoaded(true);
   }, []);
 
@@ -48,10 +58,10 @@ const Hero: React.FC<{asciiWidth: number, asciiHeight: number}> = ({asciiWidth, 
     <div className="flex items-center h-[calc(100vh-40px)]">
       <div className="flex flex-col justify-center p-10 border-2 border-grey-light lg:border-0 bg-grey-dark lg:bg-grey-normal lg:w-1/3">
         <div className="text-6xl">
-          <h1 className="animate-fade-up">Hello,</h1>
-          <h1 className={`${loaded ? "opacity-1" : "opacity-0"} animate-fade-up`} style={{animationDelay: "200ms"}}>I&apos;m <span className="text-orange">John</span></h1>
+          <h1 className={`${loaded ? "opacity-1" : "opacity-0"} animate-fade-up`} style={{animationDelay: "200ms"}} >Hello,</h1>
+          <h1 className={`${loaded ? "opacity-1" : "opacity-0"} animate-fade-up`} style={{animationDelay: "400ms"}}>I&apos;m <span className="text-orange">John</span></h1>
         </div>
-        <div className={`mt-12 ${loaded ? "opacity-1" : "opacity-0"} animate-fade-up`} style={{animationDelay: "400ms", animationDuration: "1s"}}>
+        <div className={`mt-12 ${loaded ? "opacity-1" : "opacity-0"} animate-fade-up`} style={{animationDelay: "600ms", animationDuration: "1s"}}>
           <p className="mb-5">I make things.</p>
           <p className="mb-5">I&apos;m a computer programmer who enjoys staring at screens for most of their day in order to make cool things for themselves and others.</p>
           <p className="hidden mb-5 md:block">Computers are super interesting (in my opinion) so I study Computer Science at the University of Melbourne.</p>
