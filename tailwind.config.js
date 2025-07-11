6cd707ec0ba5568f1d9bfcb83e47b0244aa4a6c2
John Ling <johnlingbusiness@gmail.com>
2025-07-11 21:19:26 +0800

    refactor: Added tailwind css to project
    - Moved css files into src directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
-// /** @type {import('tailwindcss').Config} */
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: "selector",
   theme: {
       extend: {},
@@ -62,5 +57,3 @@ const config: Config = {
   },
   plugins: [],
 }
-
-export default config;
