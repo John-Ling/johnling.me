becb4acf8636954b61abee01d45b789eab342857
John Ling <johnlingbusiness@gmail.com>
2025-07-15 14:05:46 +1000

    chore: removed useless projects from projects page

--- components/projects/projects.ts
+++ components/projects/projects.ts
@@ -9,7 +9,7 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Krux Infra", dateRange: "November 2024 - February 2025",
+    title: "Krux Infra (Internship Project 2)", dateRange: "November 2024 - February 2025",
     description: `
       Completed as part of my internship project to build the frontend for a trade application system. I collaborated with both a graphic designer and
       worked under the supervision of a senior engineer (both very cool guys). I picked up Angular and Formly in order to build the interface and got to brush up on my
@@ -37,15 +37,15 @@ export const projects: Project[] = [
       sourceURL: "https://github.com/John-Ling/CS-Fundamentals",
       tags: ["C", "Make"],
     }, 
-    {
-      title: "More Algorithms and Data Structures", dateRange: "August 2024 - October 2024",
-      description: `
-        Spent most of the time studying and dealing with life stuff but killed some time implementing all the algorithms
-        and data structures covered in my DSA course.`,
-      imageFolder: "more-dsa",
-      sourceURL: "https://github.com/John-Ling/CS-Fundamentals",
-      tags: ["C"],
-    },
+    // {
+    //   title: "More Algorithms and Data Structures", dateRange: "August 2024 - October 2024",
+    //   description: `
+    //     Spent most of the time studying and dealing with life stuff but killed some time implementing all the algorithms
+    //     and data structures covered in my DSA course.`,
+    //   imageFolder: "more-dsa",
+    //   sourceURL: "https://github.com/John-Ling/CS-Fundamentals",
+    //   tags: ["C"],
+    // },
     {
       title: "ramble.johnling.me", dateRange: "April 2024 - April 2024",
       description: `
@@ -68,9 +68,9 @@ export const projects: Project[] = [
       
     },
     {
-      title: "Internship Project", dateRange: "November 2023 - November 2023",
+      title: "RAG System (Internship Project 1)", dateRange: "November 2023 - November 2023",
       description: `
-        Fullstack web app that allowed users ask questions and received tailored financial advice 
+        Fullstack web app and RAG system that allowed users ask questions and received tailored financial advice 
         for over 1000 Malaysian companies. I used Flask and React with MySQL to store \"metadata\" 
         about companies. Used Docker but never ended up deploying.`,
       imageFolder: "internship-1",
@@ -86,15 +86,15 @@ export const projects: Project[] = [
       imageFolder: "wikihopper",
       tags: ["React", "Sass"],
     },
-    {
-      title: "Arduino Smart Lock", dateRange: "June 2023 - September 2023",
-      description: `
-        Created a low-power ATmega328p system that that used RFID to lock and unlock doors
-        (my door specifically). Learned a bit about power saving in embedded systems which was cool.`,
-      sourceURL: "https://github.com/John-Ling/Arduino-Smart-Lock",
-      imageFolder: "smart-lock",
-      tags: ["ATmega328p", "Arduino", "Hardware"],
-    },
+    // {
+    //   title: "Arduino Smart Lock", dateRange: "June 2023 - September 2023",
+    //   description: `
+    //     Created a low-power ATmega328p system that that used RFID to lock and unlock doors
+    //     (my door specifically). Learned a bit about power saving in embedded systems which was cool.`,
+    //   sourceURL: "https://github.com/John-Ling/Arduino-Smart-Lock",
+    //   imageFolder: "smart-lock",
+    //   tags: ["ATmega328p", "Arduino", "Hardware"],
+    // },
     {
       title: "Trello Tabliss Integration", dateRange: "January 2023 - May 2023",
       description: `
@@ -115,13 +115,13 @@ export const projects: Project[] = [
       sourceURL: "https://github.com/John-Ling/Pseudocode-Compiler",
       tags: ["C++"],
     },
-    {
-      title: "johnling.me", dateRange: "June 2022 - August 2022",
-      description: `Personal website about me and my projects. Built using Bootstrap, Sass and a smattering of Javascript.`,
-      sourceURL: "https://github.com/John-Ling/johnling.me",
-      imageFolder: "johnling",
-      tags: ["Bootstrap", "Sass", "Javascript", "NGINX"],
-    },
+    // {
+    //   title: "johnling.me", dateRange: "June 2022 - August 2022",
+    //   description: `Personal website about me and my projects. Built using Bootstrap, Sass and a smattering of Javascript.`,
+    //   sourceURL: "https://github.com/John-Ling/johnling.me",
+    //   imageFolder: "johnling",
+    //   tags: ["Bootstrap", "Sass", "Javascript", "NGINX"],
+    // },
     {
       title: "Cameraman", dateRange: "November 2021 - February 2022",
       description: `Uses microcontrollers, 
@@ -151,56 +151,56 @@ export const projects: Project[] = [
       imageFolder: "kahoot-python",
       tags: ["Python", "Selenium"],
     },
-    {
-      title: "CS Fundamentals", dateRange: "August 2021 - Present",
-      description: `
-        Ongoing code implementations of various data structures and algorithms in C. 
-        Absolutely saved me during my algorithms course in University.`,
-      sourceURL: "https://github.com/John-Ling/CS-Fundamentals",
-      imageFolder: "fundamentals",
-      tags: ["C"],
-    },
-    {
-      title: "CS50", dateRange: "August 2021 - August 2022",
-      description: `
-        Completed Harvard's CS50 intro to computer science course while doing school.
-        It was pretty hard but I learned a lot.
-      `,
-      sourceURL: null,
-      imageFolder: "cs50",
-      tags: ["C", "Python", "SQL", "HTML", "Javascript", "CSS"]
-    },
-    {
-      title: "Linux Shenanigians", dateRange: "May 2020 - Present",
-      description:  `
-        Had time thanks for a worldwide pandemic so I installed Arch Linux and KDE plasma onto my computers.
-        Learned a lot about Linux and had fun ricing my system.
-        I even got GPU passthrough working on my system to run both Windows and macOS virtual machines with discrete graphics.
-        Learned to compile my own kernel to get things working (ACS override).
-        Spent way too much time I could have spent socialising but I had fun :)
-      `,
-      sourceURL: null,
-      imageFolder: "linux",
-      tags: ["Bash", "Linux", "KVM", "VFIO", "I", "Use", "Arch", "BTW"]
-    },
-    {
-      title: "Covid Companion", dateRange: "February 2020 - March 2020",
-      description: `
-        Created for a \"code jam\" hosted by Replit back when we had nothing but time. 
-        Tkinter desktop app that answered queries related to COVID using NLP. 
-        I used Wit AI to determine intent and provided correct information.`,
-      sourceURL: "https://github.com/John-Ling/Covid-Companion",
-      imageFolder: "companion",
-      tags: ["Python", "Tkinter", "BeautifulSoup"],
-    },
-    {
-      title: "The Kahoot Bot™", dateRange: "September 2019 - September 2019",
-      description: `
-        The Original Kahoot Bot and my first project ever. 
-        Filled with top quality code such as unintentional recursion, Singleton design (i used globals everywhere)
-        and a codebase that boldly rejects OOP (i didn't know how to use classes).`,
-      sourceURL: null,
-      imageFolder: "kahoot-original",
-      tags: ["Python", "Selenium"],
-    }
+    // {
+    //   title: "CS Fundamentals", dateRange: "August 2021 - Present",
+    //   description: `
+    //     Ongoing code implementations of various data structures and algorithms in C. 
+    //     Absolutely saved me during my algorithms course in University.`,
+    //   sourceURL: "https://github.com/John-Ling/CS-Fundamentals",
+    //   imageFolder: "fundamentals",
+    //   tags: ["C"],
+    // },
+    // {
+    //   title: "CS50", dateRange: "August 2021 - August 2022",
+    //   description: `
+    //     Completed Harvard's CS50 intro to computer science course while doing school.
+    //     It was pretty hard but I learned a lot.
+    //   `,
+    //   sourceURL: null,
+    //   imageFolder: "cs50",
+    //   tags: ["C", "Python", "SQL", "HTML", "Javascript", "CSS"]
+    // },
+    // {
+    //   title: "Linux Shenanigians", dateRange: "May 2020 - Present",
+    //   description:  `
+    //     Had time thanks for a worldwide pandemic so I installed Arch Linux and KDE plasma onto my computers.
+    //     Learned a lot about Linux and had fun ricing my system.
+    //     I even got GPU passthrough working on my system to run both Windows and macOS virtual machines with discrete graphics.
+    //     Learned to compile my own kernel to get things working (ACS override).
+    //     Spent way too much time I could have spent socialising but I had fun :)
+    //   `,
+    //   sourceURL: null,
+    //   imageFolder: "linux",
+    //   tags: ["Bash", "Linux", "KVM", "VFIO", "I", "Use", "Arch", "BTW"]
+    // },
+    // {
+    //   title: "Covid Companion", dateRange: "February 2020 - March 2020",
+    //   description: `
+    //     Created for a \"code jam\" hosted by Replit back when we had nothing but time. 
+    //     Tkinter desktop app that answered queries related to COVID using NLP. 
+    //     I used Wit AI to determine intent and provided correct information.`,
+    //   sourceURL: "https://github.com/John-Ling/Covid-Companion",
+    //   imageFolder: "companion",
+    //   tags: ["Python", "Tkinter", "BeautifulSoup"],
+    // },
+    // {
+    //   title: "The Kahoot Bot™", dateRange: "September 2019 - September 2019",
+    //   description: `
+    //     The Original Kahoot Bot and my first project ever. 
+    //     Filled with top quality code such as unintentional recursion, Singleton design (i used globals everywhere)
+    //     and a codebase that boldly rejects OOP (i didn't know how to use classes).`,
+    //   sourceURL: null,
+    //   imageFolder: "kahoot-original",
+    //   tags: ["Python", "Selenium"],
+    // }
   ];
