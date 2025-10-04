1cfac5bec987d2bf2ab526c464b1dd8cffb7a952
John Ling <johnlingbusiness@gmail.com>
2025-10-04 21:40:38 +1000

    feat: testing husky
    - added husky and lint-staged

--- components/blog/index.tsx
+++ components/blog/index.tsx
@@ -1,29 +1,41 @@
-import { get_sorted_posts } from "@/lib/posts";
-import { meslo } from "@/lib/font";
-import Link from "next/link";
+import { get_sorted_posts } from '@/lib/posts';
+import { meslo } from '@/lib/font';
+import Link from 'next/link';
 
 export default function BlogPage() {
   const posts: BlogPost[] = get_sorted_posts();
   return (
     <>
       <title>Blog</title>
-      <div className="flex flex-col items-center min-h-screen p-5">
-        <h1 className={`text-3xl animate-fade-up opacity-0 ${meslo.variable} font-meslo`} style={{animationDelay: "100ms"}}>
-          Do People Still 
-          <span className="text-[#2e2e2e] animate-flicker-on" style={{animationDelay: "1000ms"}}> Blog? </span>
+      <div className='flex flex-col items-center min-h-screen p-5'>
+        <h1
+          className={`text-3xl animate-fade-up opacity-0 ${meslo.variable} font-meslo`}
+          style={{ animationDelay: '100ms' }}
+        >
+          Do People Still
+          <span className='text-[#2e2e2e] animate-flicker-on' style={{ animationDelay: '1000ms' }}>
+            {' '}
+            Blog?{' '}
+          </span>
         </h1>
-        <p className="opacity-0 animate-fade-up" style={{animationDelay: "150ms"}}>Inconsistently posted ramblings of consistently low quality.</p>
-        <div className="m-5 w-full md:w-3/5 lg:w-2/5 opacity-0 animate-fade-up">
+
+        <p className='opacity-0 animate-fade-up' style={{ animationDelay: '150ms' }}>
+          Inconsistently posted ramblings of consistently low quality.
+        </p>
+        <div className='m-5 w-full md:w-3/5 lg:w-2/5 opacity-0 animate-fade-up'>
           <ul>
-          {posts.map((post: BlogPost, i: number) => {
+            {posts.map((post: BlogPost, i: number) => {
               return (
-                <li key={post.slug}  className="bg-grey-card p-3 md:p-5 m-3 border-2 border-grey-light opacity-0 animate-fade-up"
-                  style={{animationDelay: `${(i + 1) * 100}ms`}}
+                <li
+                  key={post.slug}
+                  className='bg-grey-card p-3 md:p-5 m-3 border-2 border-grey-light opacity-0 animate-fade-up'
+                  style={{ animationDelay: `${(i + 1) * 100}ms` }}
                 >
                   <PostComponent position={i} post={post} />
-                </li> 
-            )})}
-          </ul> 
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </>
@@ -35,14 +47,24 @@ interface PostComponentProps {
   position: number;
 }
 
-function PostComponent({post, position}: PostComponentProps) {
-  const colours: string[] = ["text-blue", "text-magenta", "text-teal", "text-green", "text-yellow", "text-orange-light", "text-red"];
+function PostComponent({ post, position }: PostComponentProps) {
+  const colours: string[] = [
+    'text-blue',
+    'text-magenta',
+    'text-teal',
+    'text-green',
+    'text-yellow',
+    'text-orange-light',
+    'text-red'
+  ];
   const colourClass: string = colours[position % colours.length];
   return (
     <>
       <h2 className={`text-md ${meslo.variable} font-meslo ${colourClass}`}>{post.title}</h2>
-      <p className="text-sm italic text-muted-white mb-2 ">{post.date}</p>
-      <Link className="link" href={`/blog/${post.slug}`}>Article</Link>
+      <p className='text-sm italic text-muted-white mb-2 '>{post.date}</p>
+      <Link className='link' href={`/blog/${post.slug}`}>
+        Article
+      </Link>
     </>
-  )
+  );
 }
