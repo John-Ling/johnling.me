6bedf953904daa870d103840c2bc88748c7a8ab7
John Ling <johnlingbusiness@gmail.com>
2025-12-10 11:58:13 +1100

    chore: changed fonts to woff2 to hopefully fight loading problems

--- lib/font.ts
+++ lib/font.ts
@@ -3,22 +3,22 @@ import localFont from "next/font/local";
 export const caskaydia = localFont({
   src: [
     {
-      path: "../public/fonts/CaskaydiaMonoNerdFontMono-Regular.ttf",
+      path: "../public/fonts/CaskaydiaMonoNerdFontMono-Regular.woff2",
       weight: "400",
       style: "normal"
     },
     {
-      path: "../public/fonts/CaskaydiaMonoNerdFontMono-Bold.ttf",
+      path: "../public/fonts/CaskaydiaMonoNerdFontMono-Bold.woff2",
       weight: "700",
       style: "normal"
     },
     {
-      path: "../public/fonts/CaskaydiaMonoNerdFontMono-Italic.ttf",
+      path: "../public/fonts/CaskaydiaMonoNerdFontMono-Italic.woff2",
       weight: "400",
       style: "italic"
     },
     {
-      path: "../public/fonts/CaskaydiaMonoNerdFontMono-BoldItalic.ttf",
+      path: "../public/fonts/CaskaydiaMonoNerdFontMono-BoldItalic.woff2",
       weight: "700",
       style: "italic"
     }
