b2d6c424952b6bd76476e685cb6bca49a8c999f3
John Ling <johnlingbusiness@gmail.com>
2026-07-10 15:21:34 +1000

    fix: reduced projects page width

--- app/(home)/hero/hero.tsx
+++ app/(home)/hero/hero.tsx
@@ -60,7 +60,7 @@ export default function Hero() {
   return (
     <motion.div variants={container} initial='hidden' animate='show'>
       <div
-        className={`min-h-[70vh] md:min-h-[85vh] max-w-[1920px] w-11/12 lg:w-11/12 2xl:w-9/12 mx-auto mt-10 flex justify-start items-center flex-col lg:flex-row relative`}
+        className={`min-h-[70vh] md:min-h-[85vh] max-w-[1920px] w-11/12 lg:w-10/12 2xl:w-8/12 mx-auto mt-10 flex justify-start items-center flex-col lg:flex-row relative`}
       >
         <div className='basis-6/12 lg:basis-7/12 2xl:basis-6/12 max-w-5xl  flex flex-col z-20 lg:-translate-y-32'>
           <div className='text-[5rem] z-0 text-left font-serif'>
--- app/projects/index.tsx
+++ app/projects/index.tsx
@@ -45,7 +45,7 @@ export default function ProjectsPage() {
         )}
       </AnimatePresence>
       <title>Projects</title>
-      <div className={`min-h-screen w-11/12 mx-auto max-w-[1920px] pt-10`}>
+      <div className={`min-h-screen w-11/12 lg:w-10/12 2xl:w-10/12 mx-auto max-w-[1920px] pt-10`}>
         <motion.h1
           initial={{ opacity: 0, transform: "translateY(8px)" }}
           animate={{ opacity: 1, transform: "translateY(0px)" }}
