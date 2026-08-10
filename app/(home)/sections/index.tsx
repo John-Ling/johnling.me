84bee30925c97ae59342c42faac379f7bc0f9949
John Ling <johnlingbusiness@gmail.com>
2026-08-10 23:26:24 +1000

    feat: add about section in navbar

--- app/(home)/sections/index.tsx
+++ app/(home)/sections/index.tsx
@@ -6,7 +6,7 @@ export default function HomeSections() {
 	return (
 		<div className="flex flex-col items-center mx-auto">
 			<div className="grid auto-rows-auto md:grid-cols-2 mx-auto w-11/12 lg:w-10/12 2xl:w-8/12 text-sm">
-				<div className="flex flex-col col-span-2">
+				<div className="flex flex-col col-span-2" id="about">
 					<About />
 					<Blog />
 				</div>
